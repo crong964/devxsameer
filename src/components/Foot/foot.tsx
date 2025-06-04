0aef03f64a556d6651407dff8142e73ebb36eb4e
crong964 <huy91027@gmail.com>
2025-06-04 16:06:59 +0700

    f

--- src/components/Foot/foot.tsx
+++ src/components/Foot/foot.tsx
@@ -1,9 +1,10 @@
+
 export default function Foot() {
     return (
         <>
             <div className="bg-[#263238] py-10 px-25 text-white">
-                <div className="grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 grid flex flex-col">
-                    <div className="col-span-1 flex flex-col space-y-8">
+                <div className="flex gap-3 flex-wrap">
+                    <div className="col-span-1 flex-1 flex flex-col space-y-8">
                         <div className="flex  items-center  space-x-3">
                             <img src="/main.png" alt="icon" />
                             <span className="text-[26px] ">
@@ -11,55 +12,50 @@ export default function Foot() {
                             </span>
                         </div>
                         <div className=" text-[13px] text-[#F5F7FA]">
-                            Copyright © 2025 Landify Ui Kit.
+                            Copyright © 2025 Landify Ui Kit. <br />
                             All rights reserved
                         </div>
-                        <div className="flex space-x-5 justify-center">
+                        <div className="flex space-x-5 justify-start">
                             <i className="ri-instagram-line rounded-[50%] px-1 py-0.5 bg-[#4A5155]"></i>
                             <i className="ri-twitter-x-fill rounded-[50%] px-1 py-0.5 bg-[#4A5155]"></i>
                             <i className="ri-youtube-line rounded-[50%] px-1 py-0.5 bg-[#4A5155]"></i>
                             <i className="ri-github-line rounded-[50%] px-1 py-0.5 bg-[#4A5155]"></i>
                         </div>
                     </div>
-                    <div className="hidden lg:block col-span-2">
-
+                    <div className="flex flex-col gap-2.5">
+                        <div className="text-lg font-bold">Company</div>
+                        <ul className="flex flex-col gap-1.5">
+                            {
+                                ["About Us",
+                                    "Blog",
+                                    "Contact Us",
+                                    "Pricing",
+                                    "Testimonials",
+                                ]
+                                    .map((v) => {
+                                        return <li className="list-none text-[11px] font-light" key={v}>
+                                            {v}
+                                        </li>
+                                    })
+                            }
+                        </ul>
                     </div>
-                    <div className="col-span-1 flex mt-4 gap-4 justify-center">
-                        <div className="flex flex-col gap-2.5">
-                            <div className="text-lg font-bold">Company</div>
-                            <ul className="flex flex-col gap-1.5">
-                                {
-                                    ["About Us",
-                                        "Blog",
-                                        "Contact Us",
-                                        "Pricing",
-                                        "Testimonials",
-                                    ]
-                                        .map((v) => {
-                                            return <li className="list-none text-[11px] font-light" key={v}>
-                                                {v}
-                                            </li>
-                                        })
-                                }
-                            </ul>
-                        </div>
-                        <div className="flex flex-col gap-2.5">
-                            <div className="text-lg font-bold gap-2.5">Support</div>
-                            <ul className="flex flex-col gap-1.5">
-                                {
-                                    ["Help Center",
-                                        "Terms of Service",
-                                        "Legal",
-                                        "Privacy Policy",
-                                        "Status"]
-                                        .map((v) => {
-                                            return <li className="list-none text-[11px] font-light" key={v}>
-                                                {v}
-                                            </li>
-                                        })
-                                }
-                            </ul>
-                        </div>
+                    <div className="flex flex-col gap-2.5">
+                        <div className="text-lg font-bold gap-2.5">Support</div>
+                        <ul className="flex flex-col gap-1.5">
+                            {
+                                ["Help Center",
+                                    "Terms of Service",
+                                    "Legal",
+                                    "Privacy Policy",
+                                    "Status"]
+                                    .map((v) => {
+                                        return <li className="list-none text-[11px] font-light" key={v}>
+                                            {v}
+                                        </li>
+                                    })
+                            }
+                        </ul>
                     </div>
                     <div className="col-span-1 flex items-center justify-center mt-4 gap-2.5 flex-col">
                         <div className="font-bold text-[18px] w-max">Stay up to date
